e0f2d8374baf558cf5900b00742a5a6665db889d
Ri-Nai <383821630@qq.com>
2024-11-06 14:01:48 +0800

    📝 修改了程序设计第29题思路及代码

--- Programming-Practice/464323.cpp
+++ Programming-Practice/464323.cpp
@@ -1,53 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef unsigned long long ull;
-const int N = 1e6 + 5;
-int num[N], cost[N], counts[N], tot;
-signed main()
+int main()
 {
+    ios::sync_with_stdio(false);
     int n;
     cin >> n;
-    // vector<int> num(n), cost(n);
+    vector<int> num(n), cost(n);
     for (int i = 0; i < n; ++i)
-        scanf("%d", &num[i]);
+        cin >> num[i];
     for (int i = 0; i < n; ++i)
-        scanf("%d", &cost[i]);
+        cin >> cost[i];
+    sort(num.begin(), num.end());
     ull ans = 0;
-    sort(num, num + n);
-    sort(cost, cost + n);
-    if (num[0] == num[n - 1])
-    {
-        for (int i = 0; i < n - 1; ++i)
-            ans += 1ll * cost[i] * (n - i - 1);
-        cout << ans << endl;
-        return 0;
-    }
-    set<int> S;
+    vector<int> counts;
+    vector<int> S;
     for (int i = 0; i < n; ++i)
     {
         int j = i;
         while ((j + 1) < n && num[j + 1] - num[i] < (j + 1) - i)
             ++j;
-        for (int k = i + 1; k <= j; ++k)
-            S.insert(num[i] + k - i);
-        for (int k = j; k >= i + 1; --k)
+        int top = num[i] + j - i;
+        for (int k = j; k >= i; --k)
         {
-            auto it = S.lower_bound(num[k]);
-            int now = *it - num[k];
-            counts[tot++] = now;
-            S.erase(it);
+            while (top >= num[k])
+                S.push_back(top--);
+            int now = S.back() - num[k];
+            S.pop_back();
+            counts.push_back(now);
         }
         i = j;
     }
-    sort(counts, counts + tot, greater<int>());
-    for (int i = 0; i < tot; ++i)
+    sort(counts.begin(), counts.end(), greater<int>());
+    sort(cost.begin(), cost.end());
+    for (int i = 0; i < counts.size(); ++i)
         ans += 1ll * counts[i] * cost[i];
     cout << ans << endl;
 }
-/*
-你有 n 个物品，每个物有一个权值a_i和费用cost_i
-你可以进行以下操作：
-选择一个 i，a_i += 1并花费cost_i
-你一开始可以将cost_i重排
-求问将物品权值变得互不相同最小花费
-*/
